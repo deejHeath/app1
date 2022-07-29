c2b72090a9e526ba4b50bc50c5a7f188243e189c
deejHeath <heathdj@plu.edu>
2022-07-29 11:21:00 -0700

    added if and counter to switch images

--- app1/ViewController.swift
+++ app1/ViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 
+var n: Int=0
+
 class ViewController: UIViewController {
     @IBOutlet weak var msg1Label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("☘️ ViewDidLoad has run.")
     }
-
-
+    
+    
     @IBAction func msgButtonPressed(_ sender: UIButton) {
         print("🍄 Msg1Button was pressed.")
-        msg1Label.text="Is done."
+        msg1Label.text="イメージ: \(n)."
+        imageView.image=UIImage(named: "image\(n)")
+        n=(n+1)%10
     }
 }
 
