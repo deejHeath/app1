b902bb334dd25277c85e7b9c62fc89dcf662e00f
deejHeath <heathdj@plu.edu>
2022-08-24 14:32:51 -0700

    bunch of func's added to clean things up

--- app1/ViewController.swift
+++ app1/ViewController.swift
@@ -8,27 +8,25 @@
 import UIKit
 import AVFoundation
 
-var imageNumber=0
+var imageNumber = -1
+var soundNumber = -1
+var messageNumber = -1
+let numberOfImages = 10
+let NumberOfSounds = 10
 
 class ViewController: UIViewController {
     @IBOutlet weak var msg1Label: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     var audioPlayer: AVAudioPlayer!
-    let messageString = ["0","1","2"]
+    let messageString = ["Hmpf.","Whatever.","Eh?"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("☘️ ViewDidLoad has run.")
     }
     
-    
-    @IBAction func msgButtonPressed(_ sender: UIButton) {
-        print("🍄 Msg1Button was pressed, imageNumber="+String(imageNumber%10)+".")
-        msg1Label.text="イメージ: \(imageNumber%10). "+messageString[imageNumber%messageString.count]
-        imageView.image=UIImage(named: "image\(imageNumber%10)")
-        let soundName="sound\(imageNumber%10)"
-        imageNumber=(imageNumber+1)
-        
+    func playSound(soundNumber: Int) {
+        let soundName="sound\(soundNumber)"
         if let sound = NSDataAsset(name: soundName){
             do {
                 try audioPlayer=AVAudioPlayer(data: sound.data)
@@ -41,5 +39,22 @@ class ViewController: UIViewController {
             print("Couldn't find sound0.")
         }
     }
+    
+    func generateRandom(oldNumber: Int, maxNumber: Int) -> Int {
+        var newNumber: Int = -1
+        repeat {
+            newNumber=Int.random(in: 0...maxNumber-1)
+        } while newNumber == oldNumber
+        
+        return newNumber
+    }
+    
+    @IBAction func msgButtonPressed(_ sender: UIButton) {
+        print("🍄 Msg1Button was pressed.")
+        msg1Label.text=messageString[generateRandom(oldNumber: messageNumber, maxNumber: messageString.count)]
+        imageView.image=UIImage(named: "image\(generateRandom(oldNumber: imageNumber, maxNumber: numberOfImages))")
+        playSound(soundNumber: generateRandom(oldNumber: soundNumber, maxNumber: NumberOfSounds))
+    }
+    
 }
 
